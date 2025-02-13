e196296e646e2c8d24404f471a99dabb981852fd
unknown <tamieamakiri@gmail.com>
2025-02-13 23:16:22 +0100

    correction

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { RouterLink, RouterView } from 'vue-router';
 // import LoginView from './views/LoginView.vue';
 
 
@@ -7,6 +7,8 @@ import { RouterView } from "vue-router";
 
 <template>
   <main>
+
+    <RouterView/>
      <RouterView/>
    
   </main>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <script setup>
     import HeaderView from '@/components/HeaderView.vue';
+    import { RouterLink,RouterView } from 'vue-router';
     import FormLoginView from '@/components/FormLoginView.vue';
     import Footerview from '@/components/FooterView.vue';
 </script>
@@ -7,6 +8,7 @@
 <template>
     <main>
         <HeaderView/>
+        <h2>This is the Login Page</h2>
         <FormLoginView/>
         <Footerview/>
     </main>
